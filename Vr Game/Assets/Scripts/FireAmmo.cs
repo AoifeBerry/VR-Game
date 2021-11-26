29bbe7bedb73d9db79f5a3c380157f29f96ce0d3
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2021-11-26 13:24:33 +0000

    Added in different wand types and tags so they can change

---
+++ Vr Game/Assets/Scripts/FireAmmo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAmmo : MonoBehaviour
+{
+
+    public GameObject FireAmmo1;
+    public GameObject WaterAmmo;
+    public GameObject BasicAmmo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FireAmmo")
+        {
+            FireAmmo1.SetActive(true);
+            WaterAmmo.SetActive(false);
+            BasicAmmo.SetActive(false);
+        }
+
+        if (other.gameObject.tag == "WaterAmmo")
+        {
+            WaterAmmo.SetActive(true);
+            FireAmmo1.SetActive(false);
+            BasicAmmo.SetActive(false);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
