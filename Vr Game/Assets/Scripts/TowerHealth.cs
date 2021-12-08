f64b267161f9fcd0a52a62f0e868c2091d328667
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2021-12-08 22:34:49 +0000

    Tower has health and a health bar that goes down when an enemy hits it

---
+++ Vr Game/Assets/Scripts/TowerHealth.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyFire"))
+        {
+            Debug.Log("FireEnemy");
+            TakeDamage(5);
+        }
+        if (collision.gameObject.CompareTag("EnemyWater"))
+        {
+            Debug.Log("waterEnemy");
+            TakeDamage(5);
+        }
+        if (collision.gameObject.CompareTag("EnemyAir"))
+        {
+            Debug.Log("AirEnemy");
+            TakeDamage(5);
+        }
+    }
+    // Update is called once per frame
+ 
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+    }
+}
